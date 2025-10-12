0590d40511e6bfbd847313b66082b2840d15a1dc
Kiran-MS-web <kiranms880@gmail.com>
2025-10-12 12:04:45 +0530

    "Longest Subarray with sum K"

--- Arrays/Subarray.java
+++ Arrays/Subarray.java
@@ -1,8 +1,10 @@
+import java.util.HashMap;
+
 public class Subarray {
     public static void main(String[] args) {
         int[] arr = {2,3,5,1,9};
         int k =10;
-        int len = getLongestSubarray(arr, k);
+        int len = getLongestSubarray1(arr, k);
         System.out.println("The length of the longest subarray is: " + len);
     }
     static int getLongestSubarray(int[] arr, int k){
@@ -21,6 +23,28 @@ public class Subarray {
             if(sum == k) len = Math.max(len, right-left+1);
             right++;
             if(right<n) sum += arr[right];
+        }
+        return len;
+    }
+
+    static int getLongestSubarray1(int[] arr, int k){
+        int n = arr.length;
+        HashMap <Integer, Integer> map = new HashMap<>();
+
+        int sum=0;
+        int len =0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+            if(sum == k) len = Math.max(len,i+1);
+
+            int rem = sum -k;
+            if(map.containsKey(rem)){
+                 int mlen = i - map.get(rem);
+                len = Math.max(len, mlen);
+            }
+            if(!map.containsKey(sum)){
+                map.put(sum, i);
+            }
         }
         return len;
     }
