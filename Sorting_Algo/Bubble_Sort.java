d516132cc7209bcacf4054e247df2380e57d3b1d
Kiran-MS-web <kiranms880@gmail.com>
2025-11-21 15:25:44 +0530

    "Longest Subarray with sum K"

--- Sorting_Algo/Bubble_Sort.java
+++ Sorting_Algo/Bubble_Sort.java
@@ -1,3 +1,5 @@
+package Sorting_Algo;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -18,16 +20,16 @@ public class Bubble_Sort {
 
         //sorting
         for(int i=n-1; i>0; i--){
-            for(int j=1; j<=i-1; j++){
-                if(arr[j] >  arr[j+1] ){
+            for(int j=0; j<=i-1; j++){
+                if(arr[j] > arr[j+1] ){
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
         System.out.println("Sorted Array is: ");
         System.out.println(Arrays.toString(arr));
-        sc.close();
+//        sc.close();
     }
 }
