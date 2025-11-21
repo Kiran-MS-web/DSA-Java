3b203b58b801c4a895538a5b4c0a7006a477f19a
Kiran-MS-web <kiranms880@gmail.com>
2025-11-21 19:12:00 +0530

    Inserts in its correct order

--- Sorting_Algo/Insertion_Sort.java
+++ Sorting_Algo/Insertion_Sort.java
@@ -1,4 +1,32 @@
 package Sorting_Algo;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Insertion_Sort {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the size of array");
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<=n-1; i++){
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+        System.out.println("Sorted Array is: ");
+        System.out.println(Arrays.toString(arr));
+        sc.close();
+
+    }
 }
